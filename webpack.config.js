f74444117de62d0c55b17a74fdc72788159e0a2f
flexbusterman <generalstrejk@gmail.com>
2018-06-14 20:44:33 +0200

    Webpack almost works now

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,60 @@
+const path2 = require('path');
 module.exports = {
-  entry: {
-    App: "./app/assets/scripts/App.js",
-    Vendor: "./app/assets/scripts/Vendor.js"
-  },
+  entry: "./app/assets/scripts/App.js",
   output: {
-    path: "./app/temp/scripts",
-    filename: "[name].js"
-  },
-  module: {
-    loaders: [
-      {
-        loader: 'babel',
-        query: {
-          presets: ['es2015']
-        },
-        test: /\.js$/,
-        exclude: /node_modules/
-      }
-    ]
+    path: path2.resolve(__dirname, "./app/temp/scripts"),
+    filename: "App.js"
   }
+
+  // entry: "./app/assets/scripts/Vendor.js",
+  // output: {
+  //   path: path2.resolve(__dirname, "./app/temp/scripts"),
+  //   filename: "Vendor.js"
+  // }
+
 }
+
+// const path = require('path');
+
+// module.exports = {
+//   entry: {
+//     App: "./app/assets/scripts/App.js",
+//     Vendor: "./app/assets/scripts/Vendor.js"
+//   },
+//   output: {
+//     path: path.resolve(__dirname, "./app/temp/scripts"),
+//     filename: "[name].js"
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         loader: 'babel',
+//         query: {
+//           presets: ['es2015']
+//         },
+//         test: /\.js$/,
+//         exclude: /node_modules/
+//       }
+//     ]
+//   }
+// }
+
+// // frågan
+// module.exports = {
+//  entry: __dirname + "/app/assets/scripts/App.js",
+//  output: {
+//  path: __dirname + "/app/temp/scripts",
+//  filename: "App.js"
+//  }
+// }
+
+
+// // // svaret
+// const path = require('path');
+// module.exports = {
+//   entry: "./app/assets/scripts/App.js",
+//   output: {
+//     path: path.resolve(__dirname, "./app/temp/scripts"),
+//     filename: "App.js"
+//     }
+// }
