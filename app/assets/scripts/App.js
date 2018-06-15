166aafc79509f4b3ef7cc50ab9e5cf1495e125fd
flexbusterman <generalstrejk@gmail.com>
2018-06-15 15:23:38 +0200

    Git fail

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,38 +1,11 @@
-import $ from "jquery"
-import MobileMenu from "./modules/MobileMenu.js"
-import RevealOnScroll from "./modules/RevealOnScroll.js"
-import StickyHeader from "./modules/StickyHeader.js"
-import Modal from "./modules/Modal.js"
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
-var mobileMenu = new MobileMenu()
-var stickyHeader = new StickyHeader()
-var modal = new Modal()
-
-new RevealOnScroll($(".feature-item"), "85%")
-new RevealOnScroll($(".testimonial"), "60%")
-
-
-// var $ = require("jquery")
-
-// // ES5 import
-// // require don't need .js extension, but chose to include it to make it easier to understand what's going on
-// // var Person = require("./modules/Person.js")
-
-// // ES6 import
-// import Person from "./modules/Person"
-
-// // inheritance in ES6 classes
-// class Adult extends Person {
-// 	payTaxes() {
-// 		console.log(this.name + " now owes 0SEK in taxes.")
-// 	}
-// }
-
-// var john = new Person("John Doe", "blue")
-// john.greet()
-
-// var jane = new Adult("Jane Smith", "orange")
-// jane.greet()
-// jane.payTaxes()
-
-// $("h1").remove()
+var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,56 @@
-var path = require("path")
-
+const path2 = require('path');
 module.exports = {
-	entry: {
-		App: "./app/assets/scripts/App.js",
-		Vendor: "./app/assets/scripts/Vendor.js"
-	},
-	output: {
-		// webpack needs an absolute path, therefore we utilize the path.resolve
-		path: path.resolve(__dirname, "./app/temp/scripts"),
-		// [name] is dynamic and can store multiple files
-		filename: "[name].js"
-	},
-	module: {
-		loaders: [
-			{
-				loader: "babel-loader",
-				query: {
-					presets: ["es2015"]
-				},
-				test: /\.js$/,
-				exclude: /node_modules/
-			}
-		]
-	}
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js",
+  },
+  output: {
+    path: path2.resolve(__dirname, "./app/temp/scripts"),
+    filename: "[name].js"
+  }
 }
+
+// const path = require('path');
+
+// module.exports = {
+//   entry: {
+//     App: "./app/assets/scripts/App.js",
+//     Vendor: "./app/assets/scripts/Vendor.js"
+//   },
+//   output: {
+//     path: path.resolve(__dirname, "./app/temp/scripts"),
+//     filename: "[name].js"
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         loader: 'babel',
+//         query: {
+//           presets: ['es2015']
+//         },
+//         test: /\.js$/,
+//         exclude: /node_modules/
+//       }
+//     ]
+//   }
+// }
+
+// // frågan
+// module.exports = {
+//  entry: __dirname + "/app/assets/scripts/App.js",
+//  output: {
+//  path: __dirname + "/app/temp/scripts",
+//  filename: "App.js"
+//  }
+// }
+
+
+// // // svaret
+// const path = require('path');
+// module.exports = {
+//   entry: "./app/assets/scripts/App.js",
+//   output: {
+//     path: path.resolve(__dirname, "./app/temp/scripts"),
+//     filename: "App.js"
+//     }
+// }
