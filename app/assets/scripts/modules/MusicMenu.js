e347a40bdf52df2e33d824f260ce24cd7729d954
flexbusterman <generalstrejk@gmail.com>
2018-06-18 03:32:10 +0200

    Finished music section content.

--- app/assets/scripts/modules/MusicMenu.js
+++ app/assets/scripts/modules/MusicMenu.js
@@ -1,29 +1,29 @@
 import $ from 'jquery';
 
-var content = {};
-var bands = {};
-
-// bands.augustin = ;
-
-content.chicagojazzen = 'Chicagojazzen is basically the multifaceted Erik Karlsson. Based in Skellefteå, he has released no less than eight albums since 2006 - digitally, on tape and on disc, on underground labels like Zeon Light, Periferin and Irrlicht. It all began with Erik getting an USB-microphone as Christmas gift. Since then, he has refused to relate to any genre boundaries in his multi-instrumental music creation. As a red thread we find instead a messy, brutal DIY-spirit combined with references to Västerbotten.<br>Today, Chicagojazzen has evolved into a loosely cohesive collective, and among his references, Karlsson mentions Arvo Pärt, Wu Tang Clan, Fela Kuti and Darkthrone.<br><br><a href="https://www.facebook.com/chicagojazzen/" target="_blank">Facebook</a><br><a href="https://soundcloud.com/chicagojazzen" target="_blank">SoundCloud</a><br><a href="http://c-h-i-c-a-g-o-j-a-z-z-e-n.blogspot.com" target="_blank">BlogSpot</a>';
-
 $(function() {
-	// var textContainers = $("music-text-row");
 	var textContainers = $("div.music-text-row");
+	var images = $('div.music').children('img');
 	var links = $('a.link').click(function() {
 		
 		var currentLinkTxt = $(this).text().toLowerCase().split(" ");
 		var currentLinkIndex = $('a.link').index(this)
 	
+		// update link classes
 		links.removeClass('link-active');
 		$(this).addClass('link-active');
 		
+		// update music text rows classes
 		textContainers.removeClass("row--visible")
 		textContainers.addClass("row--hidden")
-
 		$(textContainers[currentLinkIndex]).removeClass("row--hidden")
 		$(textContainers[currentLinkIndex]).addClass("row--visible")
-		console.log(textContainers[currentLinkIndex])
+
+		console.log(images[currentLinkIndex])
+
+		// update images classes
+		images.removeClass("music__images-opaque")
+		$(images[currentLinkIndex]).addClass("music__images-opaque");
+
    });
 
 
