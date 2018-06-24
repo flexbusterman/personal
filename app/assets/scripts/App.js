abb79f1adb6957b51c9686c6f7b583abb155d649
flexbusterman <generalstrejk@gmail.com>
2018-06-24 22:06:19 +0200

    Completed mobile styling

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,9 +7,12 @@ import "./modules/MusicMenu";
 import "./modules/Code";
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".intro__sign"), "50%");
 new RevealOnScroll($(".code"), "85%");
 new RevealOnScroll($(".music-links"), "85%");
-// new RevealOnScroll($(".testimonial"), "60%");
+// new RevealOnScroll($(".discography-reveal-trigger"), "85%");
+new RevealOnScroll($(".about-reveal-trigger"), "85%");
+
 var stickyHeader = new StickyHeader();
 var modal = new Modal();
 // var musicMenu = new MusicMenu();
